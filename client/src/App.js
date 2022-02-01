694e4c59333b2827dabacf259a5adcd961abe9b1
aaalace <tyj5resd@gmail.com>
2022-02-01 17:22:59 +0300

    start settings

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function App() {
+  return (
+    <div className="app">
+      <p>
+        here will be our react components
+      </p>
+    </div>
+  );
+}
+
+export default App;
