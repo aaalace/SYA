9fcd4355b8122f813563d31005e3970960f8b130
Shamil <toshamilgis@gmail.com>
2022-02-09 01:01:58 +0300

    background_image_home_page

--- client/src/routes/HomePage/index.js
+++ client/src/routes/HomePage/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './home_page.css'
+import './home_page.css';
 
 export const HomePage = () => {
     return ( 
-        <div className="container">
-            
+        <div className="container" style={{position: 'relative'}}>
+            {/* <img src={require('../../images/seminar-art.png')} className='background'/> */}
         </div>
     )
 }
