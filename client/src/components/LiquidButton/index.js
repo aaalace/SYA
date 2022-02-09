8029c2d71d493355c7ba1d2708eaa4ea513fbcb4
Shamil <toshamilgis@gmail.com>
2022-02-09 13:15:26 +0300

    liquid_button and scrolling

---
+++ client/src/components/LiquidButton/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+import LiquidButton from '../LiquidButtonFromNodes/index'
+
+export const Button = () => {
+    return (
+        <LiquidButton class="browse-posts" name="Browse posts ↯" background="#AC80C1" 
+            firstcolor="#AC80C1" secondcolor="#483D8B" />
+    )
+}
