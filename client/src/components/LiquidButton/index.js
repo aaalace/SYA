53ad8144d89f4ab8e389c1f98cdebca694908307
aaalace <tyj5resd@gmail.com>
2022-02-19 20:57:54 +0300

    123

--- client/src/components/LiquidButton/index.js
+++ client/src/components/LiquidButton/index.js
@@ -3,8 +3,8 @@ import LiquidButton from '../LiquidButtonFromNodes/index'
 
 export const Button = ({text}) => {
     return (
-        // <button className="button__sign browse-posts" style={{verticalAlign: 'middle'}}><span>Log In </span></button>
-        <LiquidButton class="browse-posts" name={text} background="#AC80C1" 
-        firstcolor="#AC80C1" secondcolor="#483D8B" />
+        <button className="button__sign browse-posts" style={{verticalAlign: 'middle'}}><span>Log In </span></button>
+        // <LiquidButton class="browse-posts" name={text} background="#AC80C1" 
+        // firstcolor="#AC80C1" secondcolor="#483D8B" />
     )
 }
