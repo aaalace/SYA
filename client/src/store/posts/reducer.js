77e9601953e48ddff5d3e7447498020a69dc7476
Shamil <toshamilgis@gmail.com>
2022-03-06 14:54:56 +0300

    reducer-structure

--- client/src/store/posts/reducer.js
+++ client/src/store/posts/reducer.js
@@ -3,6 +3,33 @@ import { DELETE_POST } from "./actions"
 import { CHANGE_LIKE } from "./actions"
 import { CHANGE_BOOKMARK } from "./actions"
 
+// const mainPagePosts = {
+//     postId1: {...},
+//     postId2: {...},
+//     ...
+// }
+
+// const PostsPage = {
+//     postId1: {...},
+//     postId2: {...},
+//     ...
+// }
+
+// const UsersPagePosts = {
+//     userid1: {
+//         postId1: {...},
+//         postId2: {...},
+//         ...
+//     }, 
+//     userid2: {
+//         postId1: {...},
+//         postId2: {...},
+//         ...
+//     }, 
+//     ...
+// }
+
+
 const initialState = []
 
 export const postsReducer = (state = initialState, action) => {
