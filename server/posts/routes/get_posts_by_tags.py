e6058488da3acfc8095166e5c06ce98e790a434d
JIIMONCHIK <artmvas38@gmail.com>
2022-03-26 12:10:17 +0300

    tags done

--- server/posts/routes/get_posts_by_tags.py
+++ server/posts/routes/get_posts_by_tags.py
@@ -1,8 +1,8 @@
 from models.posts import Posts
+from models.post_tags import Post_tags
 from random import choices as chc
 from random import shuffle as shf
 from flask import request
-import json
 
 
 def get_posts_by_tags():
@@ -11,6 +11,8 @@ def get_posts_by_tags():
             res = {}
             media_ids = {}
             u_t = request.args.get('userTags', 'SYA').split('`')
+            if len(u_t) == 0:
+                u_t = chc(Post_tags.tag.all(), k = 50)
             postIds = request.args.get('postIds', '1`2').split('`')
             count = int(request.args.get('count', 6))
             if postIds[0] == '':
@@ -30,7 +32,7 @@ def get_posts_by_tags():
                 posts_with_tag = Posts.query.filter(Posts.tags.like('%' + tag + '%')).all()
                 id_set = set()
                 for p in posts_with_tag:
-                    id_set.add(p.id) 
+                    id_set.add(p.id)
                 available_count = len(id_set - set(map(int, postIds)))
                 if need_count <= available_count:
                     for _ in range(need_count):
@@ -46,7 +48,19 @@ def get_posts_by_tags():
                             pot_post = chc(posts_with_tag)[0]
                         posts_n.append(pot_post)
                         postIds.append(pot_post.id)
+                    all_posts = Posts.query.all()
+                    if need_count - available_count < len(all_posts) - len(postIds):
+                        extra_count = need_count - available_count
+                    else:
+                        extra_count = len(all_posts) - len(postIds)
+                    for _ in range(extra_count):
+                        pot_post = chc(all_posts)[0]
+                        while pot_post.id in postIds:
+                            pot_post = chc(all_posts)[0]
+                        posts_n.append(pot_post)
+                        postIds.append(pot_post.id)
                     # Можно вызывать функцию заново для числа недостающих постов
+            print(posts_n)
             shf(posts_n)
             for post in posts_n:
                 res[f'{post.id}i'] = {
