8dc6aae56347d697349817f46fedb4f54c0e74c6
Shamil <toshamilgis@gmail.com>
2022-04-10 00:58:27 +0300

    svg

--- client/src/components/AllPostsWithLazyLoading/index.js
+++ client/src/components/AllPostsWithLazyLoading/index.js
@@ -82,10 +82,10 @@ export const Posts = allPagePostsConnect(({postsConnect, setPosts}) => {
                             display: 'flex', justifyContent: 'flex-start'
                         }}>
                             <span>Перейти</span>
-                            <sv width="15px" height="10px" viewBox="0 0 13 10">
+                            <svg width="15px" height="10px" viewBox="0 0 13 10">
                                 <path d="M1,5 L11,5"></path>
                                 <polyline points="8 1 12 5 8 9"></polyline>
-                            </sv>
+                            </svg>
                         </button>
                     </div>
                 )
