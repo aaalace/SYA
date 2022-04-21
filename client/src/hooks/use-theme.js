bbdc79666a281b852666cafa1ecd6191eb841801
Shamil <toshamilgis@gmail.com>
2022-04-21 17:08:33 +0300

    theme-updates-local-storage

--- client/src/hooks/use-theme.js
+++ client/src/hooks/use-theme.js
@@ -1,13 +1,19 @@
-import {useLayoutEffect, useState} from "react";
+import { useLayoutEffect, useState } from 'react'
+
+const isDarkTheme = window?.matchMedia('(prefers-color-scheme: dark)').matches
+const defaultTheme = isDarkTheme;
 
 export const useTheme = () => {
-    const [theme, setTheme] = useState(false)
+    const [theme, setTheme] = useState(
+        localStorage.getItem('app-theme') || defaultTheme
+    )
     // false - light
     // true - dark
 
     useLayoutEffect(() => {
         document.documentElement.setAttribute('data-theme', theme ? 'dark': 'light')
+        localStorage.setItem('app-theme', theme)
     }, [theme])
 
-    return { theme, setTheme }
+    return {theme, setTheme}
 }
